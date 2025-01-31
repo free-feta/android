9e48a6fbf905d916faab3b1f1886fcd685a8c187
fira-pro <addisdebebe79@gmail.com>
2025-01-31 14:34:15 +0300

    update column name to use snake case

--- app/src/main/java/et/fira/freefeta/model/Media.kt
+++ app/src/main/java/et/fira/freefeta/model/Media.kt
@@ -1,5 +1,6 @@
 package et.fira.freefeta.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -8,13 +9,20 @@ data class Media(
     @PrimaryKey
     val id: Int,
     val isPlayable: Boolean = false,
+    @ColumnInfo(name = "media_type")
     val mediaType: MediaType = MediaType.FILE, // Needs converter and registration to DB
     val title: String,
+    @ColumnInfo(name = "download_url")
     val downloadUrl: String,
+    @ColumnInfo(name = "thumbnail_url")
     val thumbnailUlr: String? = null,
     val runtime: String? = null,
+    @ColumnInfo(name = "is_new")
     val isNew: Boolean = true,
-    val downloadId: Int? = null
+    @ColumnInfo(name = "download_id")
+    val downloadId: Int? = null,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long
 )
 
 enum class MediaType {
