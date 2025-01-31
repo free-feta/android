b8be3d8322355c2690749569bbf5b719fcd03ef7
fira-pro <addisdebebe79@gmail.com>
2025-01-31 16:20:51 +0300

    add user preference to app container

--- app/src/main/java/et/fira/freefeta/data/AppContainer.kt
+++ app/src/main/java/et/fira/freefeta/data/AppContainer.kt
@@ -1,14 +1,23 @@
 package et.fira.freefeta.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.ketch.Ketch
 import com.ketch.NotificationConfig
 import et.fira.freefeta.R
 import et.fira.freefeta.network.TeleFileDownloaderService
 
+private const val USER_PREFERENCE_NAME = "user_preferences"
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = USER_PREFERENCE_NAME
+)
+
 interface AppContainer {
     val fileDownloaderRepository: FileDownloaderRepository
     val mediaRepository: MediaRepository
+    val userPreferencesRepository: UserPreferencesRepository
 }
 
 class DefaultAppContainer(context: Context): AppContainer {
@@ -29,6 +38,9 @@ class DefaultAppContainer(context: Context): AppContainer {
 
     override val mediaRepository: MediaRepository by lazy {
         OfflineMediaRepository(FreeFetaDatabase.getDatabase(context).mediaDao())
+    }
+    override val userPreferencesRepository: UserPreferencesRepository by lazy {
+        UserPreferencesRepository(context.dataStore)
     }
 
 }
