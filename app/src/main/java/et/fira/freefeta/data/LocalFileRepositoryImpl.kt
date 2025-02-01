ff5e9f780e641714e04d67d851cf86d1a698df9f
fira-pro <addisdebebe79@gmail.com>
2025-02-01 21:40:46 +0300

    rename file from OfflineMediaRepository.kt

---
+++ app/src/main/java/et/fira/freefeta/data/LocalFileRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package et.fira.freefeta.data
+
+import et.fira.freefeta.model.FileEntity
+import kotlinx.coroutines.flow.Flow
+
+class LocalFileRepositoryImpl(
+    private val fileDao: FileDao
+): LocalFileRepository {
+    override fun getAllFilesStream(): Flow<List<FileEntity>> = fileDao.getAllFiles()
+
+    override fun getFileStream(id: Int): Flow<FileEntity?> = fileDao.getFile(id)
+
+    override suspend fun insertFile(fileEntity: FileEntity) = fileDao.insert(fileEntity)
+
+    override suspend fun deleteFile(fileEntity: FileEntity) = fileDao.delete(fileEntity)
+
+    override suspend fun updateFile(fileEntity: FileEntity) = fileDao.update(fileEntity)
+
+}
