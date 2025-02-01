0bb94508f869d7c8059390c657f042c972992fcd
fira-pro <addisdebebe79@gmail.com>
2025-02-01 21:35:32 +0300

    remove FileDownloaderRepository interface and implement it directly

--- app/src/main/java/et/fira/freefeta/data/FileDownloaderRepository.kt
+++ app/src/main/java/et/fira/freefeta/data/FileDownloaderRepository.kt
@@ -1,6 +1,17 @@
 package et.fira.freefeta.data
 
-interface FileDownloaderRepository {
-    fun download(url: String) : Int
+import android.os.Environment
+import et.fira.freefeta.network.TeleFileDownloaderService
+
+class FileDownloaderRepository(
+    private val teleFileDownloaderService: TeleFileDownloaderService
+) {
+    fun download(url: String): Int {
+        return teleFileDownloaderService.download(
+            url,
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
+        )
+    }
+
 
 }
