c82fed21c0b866a3aa2bc77e1f84cd773bf976d2
fira-pro <addisdebebe79@gmail.com>
2025-02-01 21:42:46 +0300

    refactor name and implement new repos

--- app/src/main/java/et/fira/freefeta/ui/FreeFetaViewModel.kt
+++ app/src/main/java/et/fira/freefeta/ui/FreeFetaViewModel.kt
@@ -1,5 +1,6 @@
 package et.fira.freefeta.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -8,9 +9,16 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import et.fira.freefeta.FreeFetaApplication
 import et.fira.freefeta.data.FileDownloaderRepository
-import et.fira.freefeta.data.MediaRepository
-import et.fira.freefeta.model.Media
+import et.fira.freefeta.data.LocalFileRepository
+import et.fira.freefeta.data.RemoteFileRepository
+import et.fira.freefeta.data.ThemeMode
+import et.fira.freefeta.data.UserPreferencesRepository
+import et.fira.freefeta.model.FileEntity
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 class FreeFetaViewModel(
     private val fileDownloaderRepository: FileDownloaderRepository,
@@ -19,13 +27,21 @@ class FreeFetaViewModel(
     private val userPreferencesRepository: UserPreferencesRepository
 ): ViewModel() {
 
+    val themeMode: StateFlow<ThemeMode> = userPreferencesRepository.themeMode
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = ThemeMode.SYSTEM
+        )
+
+
     init {
 //        videoFileDownloaderRepository.download("https://telebirrchat.ethiomobilemoney.et:21006/sfs/ufile?digest=fid5e4d5ff5bb0f20b7e70ec3d0bb01d1d2&filename=Khalid+-+Young+Dumb+_+Broke+(Lyrics)(720P_HD).mp4")
         viewModelScope.launch {
-            mediaRepository.insertMedia(
-                Media(
+            localFileRepository.insertFile(
+                FileEntity(
                     id = 1,
-                    title = "my media",
+                    name = "my media",
                     downloadUrl = "https://google.com",
                     createdAt = 10000000
                 )
@@ -34,15 +50,35 @@ class FreeFetaViewModel(
 
     }
 
+    fun setTheme(themeMode: ThemeMode) {
+        viewModelScope.launch {
+            userPreferencesRepository.setThemeMode(themeMode)
+        }
+    }
+
+    fun getMarsPhotos() {
+        viewModelScope.launch {
+            try {
+                val listResult = remoteFileRepository.getFiles()
+                Log.d("FreeFetaViewModel", "Success: ${listResult.toString()} Mars photos retrieved")
+
+            } catch (e: IOException) {
+                Log.e("", e.message ?: "")
+            }
+        }
+    }
+
+
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 val application = (this[APPLICATION_KEY] as FreeFetaApplication)
-                val videoFileDownloaderRepository = application.container.fileDownloaderRepository
-                val mediaRepository = application.container.mediaRepository
+                val container = application.container
                 FreeFetaViewModel(
-                    videoFileDownloaderRepository = videoFileDownloaderRepository,
-                    mediaRepository = mediaRepository
+                    fileDownloaderRepository = container.fileDownloaderRepository,
+                    localFileRepository = container.localFileRepository,
+                    remoteFileRepository = container.remoteFileRepository,
+                    userPreferencesRepository = container.userPreferencesRepository
                 )
             }
         }
