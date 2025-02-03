b7f22ee11baaef5d87d5782ee1e124e1991151cd
fira-pro <addisdebebe79@gmail.com>
2025-02-03 21:55:53 +0300

    set content to FreeFetaApp

--- app/src/main/java/et/fira/freefeta/MainActivity.kt
+++ app/src/main/java/et/fira/freefeta/MainActivity.kt
@@ -4,66 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import et.fira.freefeta.ui.FilePermissionHandler
-import et.fira.freefeta.ui.FreeFetaViewModel
-import et.fira.freefeta.ui.theme.FreeFetaTheme
+import et.fira.freefeta.ui.FreeFetaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FreeFetaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            FreeFetaApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-    freeFetaViewModel: FreeFetaViewModel = viewModel(factory = FreeFetaViewModel.Factory)
-) {
-    Column(modifier = modifier) {
-        FilePermissionHandler()
-        Text(
-            text = "Display Medium",
-            style = MaterialTheme.typography.displayMedium,
-            modifier = Modifier.padding(top = dimensionResource(id = R.dimen.padding_small))
-        )
-        Text(
-            text = "Body Large",
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Text(
-            text = "Label Medium",
-            style = MaterialTheme.typography.labelMedium
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FreeFetaTheme {
-        Greeting("Android")
     }
 }
