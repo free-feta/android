147cead24d0b1c683c5e972147c62a3ba03b735f
fira-pro <addisdebebe79@gmail.com>
2025-02-03 21:53:20 +0300

    create navigation graph for different routes

---
+++ app/src/main/java/et/fira/freefeta/ui/navigation/FreeFetaNavGraph.kt
@@ -0,0 +1,49 @@
+package et.fira.freefeta.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import et.fira.freefeta.ui.about.AboutDestination
+import et.fira.freefeta.ui.about.AboutScreen
+import et.fira.freefeta.ui.home.HomeDestination
+import et.fira.freefeta.ui.home.HomeScreen
+import et.fira.freefeta.ui.player.PlayerDestination
+import et.fira.freefeta.ui.player.PlayerScreen
+import et.fira.freefeta.ui.settings.SettingsDestination
+import et.fira.freefeta.ui.settings.SettingsScreen
+
+@Composable
+fun FreeFetaNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination.route,
+        modifier = modifier
+    ) {
+        composable(route = HomeDestination.route) {
+            HomeScreen(
+                navigateToSettings = {navController.navigate(SettingsDestination.route)},
+                navigateToAbout = {navController.navigate(AboutDestination.route)}
+            )
+        }
+        composable(route = PlayerDestination.route) {
+            PlayerScreen(
+                navController = navController
+            )
+        }
+        composable(route = SettingsDestination.route) {
+            SettingsScreen(
+                navController = navController
+            )
+        }
+        composable(route = AboutDestination.route) {
+            AboutScreen(
+                navigateBack = {navController.popBackStack()}
+            )
+        }
+    }
+}
