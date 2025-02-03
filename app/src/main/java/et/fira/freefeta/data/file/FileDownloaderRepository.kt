7dd2d8ee01dbf842c920ff68dbb12a47590454f6
fira-pro <addisdebebe79@gmail.com>
2025-02-03 21:50:37 +0300

    add observeDownloads to file downloader repo

--- app/src/main/java/et/fira/freefeta/data/file/FileDownloaderRepository.kt
+++ app/src/main/java/et/fira/freefeta/data/file/FileDownloaderRepository.kt
@@ -1,7 +1,9 @@
 package et.fira.freefeta.data.file
 
 import android.os.Environment
+import com.ketch.DownloadModel
 import et.fira.freefeta.network.TeleFileDownloaderService
+import kotlinx.coroutines.flow.Flow
 
 class FileDownloaderRepository(
     private val teleFileDownloaderService: TeleFileDownloaderService
@@ -11,6 +13,10 @@ class FileDownloaderRepository(
             url,
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).path
         )
+    }
+
+    fun observeDownloads(): Flow<List<DownloadModel>> {
+        return teleFileDownloaderService.observeDownloads()
     }
 
 
