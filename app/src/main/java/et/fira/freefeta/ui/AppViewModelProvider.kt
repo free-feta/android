43a8e2d45d0153fcba8a8299fc5abda2cc1e1ec8
fira-pro <addisdebebe79@gmail.com>
2025-02-03 21:48:09 +0300

    create viewModel provider for different screens

---
+++ app/src/main/java/et/fira/freefeta/ui/AppViewModelProvider.kt
@@ -0,0 +1,37 @@
+package et.fira.freefeta.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import et.fira.freefeta.FreeFetaApplication
+import et.fira.freefeta.ui.home.HomeViewModel
+import et.fira.freefeta.ui.player.PlayerViewModel
+import et.fira.freefeta.ui.settings.SettingsViewModel
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            SettingsViewModel(
+                userPreferencesRepository = freeFetaApplication().container.userPreferencesRepository
+            )
+        }
+
+        initializer {
+            PlayerViewModel(
+                localFileRepository = freeFetaApplication().container.localFileRepository
+            )
+        }
+
+        initializer {
+            HomeViewModel(
+                fileDownloaderRepository = freeFetaApplication().container.fileDownloaderRepository,
+                localFileRepository = freeFetaApplication().container.localFileRepository,
+                remoteFileRepository = freeFetaApplication().container.remoteFileRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.freeFetaApplication(): FreeFetaApplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FreeFetaApplication)
