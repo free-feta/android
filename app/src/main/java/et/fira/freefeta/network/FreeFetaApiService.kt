0d8ff8c10ed88d51ae756c1fbed85aed86ceaf18
fira-pro <addisdebebe79@gmail.com>
2025-02-14 00:11:56 +0300

    Add getAds endpoint to FreeFetaApiService

--- app/src/main/java/et/fira/freefeta/network/FreeFetaApiService.kt
+++ app/src/main/java/et/fira/freefeta/network/FreeFetaApiService.kt
@@ -1,11 +1,15 @@
 package et.fira.freefeta.network
 
+import et.fira.freefeta.model.Advertisement
 import et.fira.freefeta.model.FileEntity
 import retrofit2.http.GET
 
 interface FreeFetaApiService {
     @GET("files.json")
     suspend fun getFiles(): List<FileEntity>
+
+    @GET("ads.json")
+    suspend fun getAds(): List<Advertisement>
 }
 
 
