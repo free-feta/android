7ade6d95f007ab9340dd249cddbaafd6e2b8d0b6
fira-pro <addisdebebe79@gmail.com>
2025-02-14 00:10:44 +0300

    Refactor AdRepository and add new methods
    -   Refactor `AdRepository` interface and `AdRepositoryImpl` class.
    -   Add `insertAd(advertisements: List<Advertisement>)` to insert multiple advertisements.
    -   Change `getAllAds` to `getAllAdsStream` that returns a Flow.
    -   Add `fetchRemoteAds` to fetch ads from remote source.
    -   Add `syncNewAds` to synchronize new ads.
    -   Add `getStartUpAd` to get startup ad.
    -   Add `getOnDemandAd` to get on demand ad.

--- app/src/main/java/et/fira/freefeta/data/ad/AdRepository.kt
+++ app/src/main/java/et/fira/freefeta/data/ad/AdRepository.kt
@@ -1,23 +1,83 @@
 package et.fira.freefeta.data.ad
 
+import android.util.Log
 import et.fira.freefeta.model.Advertisement
+import et.fira.freefeta.network.FreeFetaApiService
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.Flow
+import kotlin.coroutines.coroutineContext
 
 interface AdRepository {
     suspend fun insertAd(advertisement: Advertisement)
+    suspend fun insertAd(advertisements: List<Advertisement>)
     suspend fun deleteAd(advertisement: Advertisement)
     fun getAd(id: Int): Flow<Advertisement>
-    fun getAllAds(): Flow<List<Advertisement>>
+    fun getAllAdsStream(): Flow<List<Advertisement>>
+    suspend fun fetchRemoteAds(): List<Advertisement>
+    suspend fun syncNewAds(): Int
+    suspend fun getStartUpAd(): Advertisement?
+    suspend fun getOnDemandAd(): Advertisement?
 }
 
 class AdRepositoryImpl(
-    private val adDao: AdDao
+    private val adDao: AdDao,
+    private val freeFetaApiService: FreeFetaApiService,
 ): AdRepository {
     override suspend fun insertAd(advertisement: Advertisement) = adDao.insert(advertisement)
+    override suspend fun insertAd(advertisements: List<Advertisement>) = adDao.insert(advertisements)
 
     override suspend fun deleteAd(advertisement: Advertisement) = adDao.delete(advertisement)
     override fun getAd(id: Int): Flow<Advertisement> = adDao.getAd(id)
 
-    override fun getAllAds(): Flow<List<Advertisement>> = adDao.getAllAds()
+    override fun getAllAdsStream(): Flow<List<Advertisement>> = adDao.getAllAdsStream()
+    override suspend fun fetchRemoteAds(): List<Advertisement> = freeFetaApiService.getAds()
+
+    override suspend fun syncNewAds(): Int {
+        try {
+            val fetchedAds = fetchRemoteAds()
+            insertAd(fetchedAds)
+            return fetchedAds.size
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+            e.printStackTrace()
+            return 0
+        }
+    }
+
+    override suspend fun getStartUpAd(): Advertisement? {
+        syncNewAds()
+        try {
+            val ads = adDao.getAllAds()
+//            Log.d("AdRepositoryImpl", "Ads filtered random: ${ads.filter { it.showOnStartup }.random()}")
+            val starUpAd = ads.filter { it.showOnStartup }.random()
+
+            if (starUpAd.isOneTime) {
+                adDao.delete(starUpAd)
+            }
+            return starUpAd
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    override suspend fun getOnDemandAd(): Advertisement? {
+        syncNewAds()
+        try {
+            val ads = adDao.getAllAds()
+            Log.d("AdRepositoryImpl", "Ads 4 on demand: $ads")
+            val onDemandAd = ads.filter { !it.showOnStartup }.random()
+            if (onDemandAd.isOneTime) {
+                adDao.delete(onDemandAd)
+            }
+            return onDemandAd
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+            e.printStackTrace()
+            return null
+        }
+
+    }
 
 }
