0360e0aca1b0b270d63ec1bf310748a52662c7bd
fira-pro <addisdebebe79@gmail.com>
2025-02-17 09:18:08 +0300

    Implement flow based App config repository with network sync functionality.

--- app/src/main/java/et/fira/freefeta/data/config/AppConfigRepository.kt
+++ app/src/main/java/et/fira/freefeta/data/config/AppConfigRepository.kt
@@ -1,21 +1,52 @@
 package et.fira.freefeta.data.config
 
+import android.util.Log
 import et.fira.freefeta.model.AppConfig
+import et.fira.freefeta.network.FreeFetaApiService
 import kotlinx.coroutines.flow.Flow
+import kotlin.coroutines.cancellation.CancellationException
 
 interface AppConfigRepository {
     suspend fun insertConfig(config: AppConfig)
     suspend fun deleteConfig(config: AppConfig)
-    fun getConfig(): Flow<AppConfig>
+    fun getConfigStream(): Flow<AppConfig?>
+    suspend fun syncConfig()
+    suspend fun getConfig(): AppConfig?
 }
 
 class AppConfigRepositoryImpl(
-    private val appConfigDao: AppConfigDao
+    private val appConfigDao: AppConfigDao,
+    private val freeFetaApiService: FreeFetaApiService
 ): AppConfigRepository {
     override suspend fun insertConfig(config: AppConfig) = appConfigDao.insert(config)
 
     override suspend fun deleteConfig(config: AppConfig) = appConfigDao.delete(config)
 
-    override fun getConfig(): Flow<AppConfig> = appConfigDao.getConfig()
+    override fun getConfigStream(): Flow<AppConfig?> = appConfigDao.getConfigStream()
+    override suspend fun syncConfig() {
+        try {
+            val fetchedConfig = freeFetaApiService.getConfig()
+            Log.d("AppConfigRepository", "Fetched config: $fetchedConfig")
+            insertConfig(fetchedConfig)
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                throw e
+            }
+            e.printStackTrace()
+        }
+    }
+    override suspend fun getConfig(): AppConfig? {
+        try {
+            syncConfig()
+            return appConfigDao.getConfig()
+        } catch (e: Exception) {
+            if (e is CancellationException) {
+                throw e
+            }
+            e.printStackTrace()
+            return null
+        }
+    }
+
 
 }
