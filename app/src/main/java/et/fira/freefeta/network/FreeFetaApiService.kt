cd1395bcb514b5a49d7aa31efb8eb007ac6bfea6
fira-pro <addisdebebe79@gmail.com>
2025-02-22 14:36:40 +0300

    Add POST method for json data upload

--- app/src/main/java/et/fira/freefeta/network/FreeFetaApiService.kt
+++ app/src/main/java/et/fira/freefeta/network/FreeFetaApiService.kt
@@ -3,7 +3,13 @@ package et.fira.freefeta.network
 import et.fira.freefeta.model.Advertisement
 import et.fira.freefeta.model.AppConfig
 import et.fira.freefeta.model.FileEntity
+import okhttp3.RequestBody
+import okhttp3.Response
+import okhttp3.ResponseBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Url
 
 interface FreeFetaApiService {
     @GET("files.json")
@@ -14,6 +20,12 @@ interface FreeFetaApiService {
 
     @GET("config.json")
     suspend fun getConfig(): AppConfig
+
+    @POST
+    suspend fun uploadJsonData(
+        @Url url: String,
+        @Body requestBody: RequestBody
+    ): ResponseBody?
 }
 
 
