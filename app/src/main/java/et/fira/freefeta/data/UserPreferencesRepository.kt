eb587db44375ecf277bc715340211df7569a5d08
fira-pro <addisdebebe79@gmail.com>
2025-02-23 01:42:16 +0300

    Refactor UserPreferencesRepository and add resize mode
    - Refactor `UserPreferencesRepository` to introduce `UserPreferences` data class, which centralizes user preferences.
    - Add a new preference, `resizeMode`, to manage video player resize behavior.
    - Add `ResizeMode` enum for player's resize mode.
    - Introduce a utility function `toExoPlayerResizeMode` to translate the `ResizeMode` enum to ExoPlayer's resize mode constants.
    - Update preference keys and add methods to read and update resize mode preference.
    - Implement the preference to complete the onboarding state.

--- app/src/main/java/et/fira/freefeta/data/UserPreferencesRepository.kt
+++ app/src/main/java/et/fira/freefeta/data/UserPreferencesRepository.kt
@@ -1,12 +1,15 @@
 package et.fira.freefeta.data
 
 import android.util.Log
+import androidx.annotation.OptIn
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.AspectRatioFrameLayout
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
@@ -40,7 +43,7 @@ class UserPreferencesRepository(
             }
         }
         .map { preferences ->
-            preferences[SHOW_DELETE_DIALOG] ?: true
+            preferences[SHOW_DELETE_DIALOG_KEY] ?: true
         }
 
     val onboardingCompleted: Flow<Boolean> = dataStore.data
@@ -53,15 +56,48 @@ class UserPreferencesRepository(
             }
         }
         .map { preferences ->
-            preferences[ONBOARDING_COMPLETED] ?: false
+            preferences[ONBOARDING_COMPLETED_KEY] ?: false
         }
+
+    val resizeMode: Flow<ResizeMode> = dataStore.data
+        .catch {
+        if(it is IOException) {
+            Log.e(TAG, "Error reading preferences.", it)
+            emit(emptyPreferences())
+        } else {
+            throw it
+        }
+    }
+    .map { preferences ->
+        ResizeMode.entries.toTypedArray().getOrElse(preferences[RESIZE_MODE_KEY] ?: ResizeMode.FIT.ordinal) {
+            ResizeMode.FIT // Default is fit
+        }
+    }
 
     companion object {
         val THEME_MODE_KEY = intPreferencesKey("theme_mode")
-        val SHOW_DELETE_DIALOG = booleanPreferencesKey("show_delete_dialog")
-        val ONBOARDING_COMPLETED = booleanPreferencesKey("onboarding_completed")
+        val SHOW_DELETE_DIALOG_KEY = booleanPreferencesKey("show_delete_dialog")
+        val ONBOARDING_COMPLETED_KEY = booleanPreferencesKey("onboarding_completed")
+        val RESIZE_MODE_KEY = intPreferencesKey("resize_mode")
         const val TAG = "UserPreferencesRepo"
     }
+
+    val userPreferences: Flow<UserPreferences> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+        .map { preferences ->
+            UserPreferences(
+                theme = preferences[THEME_MODE_KEY]?.let { ThemeMode.entries[it] } ?: ThemeMode.SYSTEM,
+                showDeleteConfirmation = preferences[SHOW_DELETE_DIALOG_KEY] ?: true,
+                resizeMode = preferences[RESIZE_MODE_KEY]?.let { ResizeMode.entries[it] } ?: ResizeMode.FIT,
+                isFirstLaunch = preferences[ONBOARDING_COMPLETED_KEY] ?: true
+            )
+        }
 
     suspend fun setThemeMode(themeMode: ThemeMode) {
         dataStore.edit {
@@ -72,13 +108,19 @@ class UserPreferencesRepository(
 
     suspend fun setShowDeleteDialog(show: Boolean) {
         dataStore.edit { preferences ->
-            preferences[SHOW_DELETE_DIALOG] = show
+            preferences[SHOW_DELETE_DIALOG_KEY] = show
         }
     }
 
-    suspend fun completeOnboarding() {
+    suspend fun completeOnboarding(completed : Boolean = true) {
         dataStore.edit { preferences ->
-            preferences[ONBOARDING_COMPLETED] = true
+            preferences[ONBOARDING_COMPLETED_KEY] = completed
+        }
+    }
+
+    suspend fun setResizeMode(resizeMode: ResizeMode) {
+        dataStore.edit { preferences ->
+            preferences[RESIZE_MODE_KEY] = resizeMode.ordinal
         }
     }
 }
@@ -88,3 +130,23 @@ enum class ThemeMode {
     DARK,
     SYSTEM
 }
+
+enum class ResizeMode {
+    FIT, FILL, ZOOM, FIXED_WIDTH, FIXED_HEIGHT,
+}
+
+@OptIn(UnstableApi::class)
+fun ResizeMode.toExoPlayerResizeMode(): Int = when (this) {
+    ResizeMode.FIT -> AspectRatioFrameLayout.RESIZE_MODE_FIT
+    ResizeMode.FILL -> AspectRatioFrameLayout.RESIZE_MODE_FILL
+    ResizeMode.FIXED_WIDTH -> AspectRatioFrameLayout.RESIZE_MODE_FIXED_WIDTH
+    ResizeMode.FIXED_HEIGHT -> AspectRatioFrameLayout.RESIZE_MODE_FIXED_HEIGHT
+    ResizeMode.ZOOM -> AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+}
+
+data class UserPreferences(
+    val theme: ThemeMode = ThemeMode.SYSTEM,
+    val showDeleteConfirmation: Boolean = true,
+    val resizeMode: ResizeMode = ResizeMode.FIT,
+    val isFirstLaunch: Boolean = true
+)
