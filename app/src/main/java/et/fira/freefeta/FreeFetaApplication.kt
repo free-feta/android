dfdeb28710824087b3e2825d9ed00762ff177c03
fira-pro <addisdebebe79@gmail.com>
2025-02-23 20:31:56 +0300

    create Image loader for coil add network interceptor class to add header to requests

--- app/src/main/java/et/fira/freefeta/FreeFetaApplication.kt
+++ app/src/main/java/et/fira/freefeta/FreeFetaApplication.kt
@@ -1,10 +1,17 @@
 package et.fira.freefeta
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.request.crossfade
 import et.fira.freefeta.data.AppContainer
 import et.fira.freefeta.data.DefaultAppContainer
+import et.fira.freefeta.util.RequestHeaderInterceptor
+import okhttp3.OkHttpClient
 
-class FreeFetaApplication: Application() {
+class FreeFetaApplication: Application(), SingletonImageLoader.Factory {
     lateinit var container: AppContainer
 
     override fun onCreate() {
@@ -12,5 +19,19 @@ class FreeFetaApplication: Application() {
         container = DefaultAppContainer(this)
 
         container.workManagerSynUpdateRepository.startSyncUpdate()
+    }
+
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+        return ImageLoader.Builder(context)
+            .components {
+                add(OkHttpNetworkFetcherFactory(
+                    callFactory = {
+                        OkHttpClient.Builder()
+                            .addNetworkInterceptor(RequestHeaderInterceptor())
+                            .build()
+                    }
+                ))
+            }
+            .build()
     }
 }
---
+++ app/src/main/java/et/fira/freefeta/util/RequestHeaderInterceptor.kt
@@ -0,0 +1,22 @@
+package et.fira.freefeta.util
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class RequestHeaderInterceptor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalUrl = originalRequest.url.toString()
+
+        if (originalUrl.contains("telebirrchat.ethiomobilemoney.et")) {
+            val modifiedRequest = originalRequest.newBuilder()
+            for (header in AppConstants.Network.HEADER_FOR_ZERO_RATING_URL) {
+                modifiedRequest.addHeader(header.key, header.value)
+            }
+            return chain.proceed(modifiedRequest.build())
+        }
+
+        return chain.proceed(originalRequest)
+    }
+}
