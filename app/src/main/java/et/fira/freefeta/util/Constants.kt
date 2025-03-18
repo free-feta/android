6731fd35d35c62c8b0aace64a4c1dc8f1a15f256
fira-pro <addisdebebe79@gmail.com>
2025-03-18 15:01:44 +0300

    change sendid in zero rating url header

--- app/src/main/java/et/fira/freefeta/util/Constants.kt
+++ app/src/main/java/et/fira/freefeta/util/Constants.kt
@@ -11,7 +11,8 @@ object AppConstants {
             "appid" to "1012673623603201",
             "access-token" to "34A1367993D9409639D081B3A91159D37FD5DF999E2B97C8C822F92E44DE6A65",
             "User-Agent" to "Dalvik/2.1.0 (Linux; U; Android 7.1.2; ASUS_Z01QD Build/N2G48H)",
-            "sendid" to "1012673623603201:978019208678401",
+//            "sendid" to "1012673623603201:978019208678401",
+            "sendid" to "1012673623603201:978374728448001",
         )
     }
 
